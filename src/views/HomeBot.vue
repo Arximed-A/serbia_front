b14b49ed4e58d2929a14dc21cc587d486de71ba2
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-10-14 10:09:50 +0300

    init

---
+++ src/views/HomeBot.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    дом
+    <button @click="onClose">закрыть</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeBot",
+  data() {
+    return {
+      tg: window.Telegram.WebApp,
+    };
+  },
+  components: {},
+  methods: {
+    onClose() {
+      this.tg.close;
+    },
+    useEffect() {
+      this.tg.ready;
+    },
+  },
+};
+</script>
+<style>
+* {
+  margin: 0px;
+  margin: 0px 0px;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background: var(--tg-theme-bg-color);
+  color: var(--tg-theme-text-color);
+}
+</style>
